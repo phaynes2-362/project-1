9b2b7dc647d3c264c9a7c81caee83ef55e62d538
paul <phaynes2@hawk.iit.edu>
2019-11-06 16:39:36 -0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,9 @@ function alert_me(){
   alert("you have submited"); 
   return true;
   }
- 
+ function validate() {
+	var x = document.forms["myForm"]["fname"].value;
+	if(x == "") {
+		alert("your must be filled out!");
+		return false;
+	}
